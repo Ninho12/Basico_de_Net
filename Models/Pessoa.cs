01beac21f0ea748bbfcd6014d167b9cbb4ba4eb4
João Paulo Sena Padilha <joaopaulojpsp@gmail.com>
2022-09-03 04:50:12 -0300

    Primeiro Commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basico_de_Net.Models
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        
+    }
+}
